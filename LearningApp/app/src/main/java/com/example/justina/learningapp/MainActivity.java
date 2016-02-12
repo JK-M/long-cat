c65c724c194c17115abcdff13770f55b4407acab
JK-M <stipriojikojyte@gmail.com>
2016-02-13 00:27:53 +0200

    Home screen (MainActivity) has been updated with the new design. Four new activities have been added.

--- LearningApp/app/src/main/java/com/example/justina/learningapp/MainActivity.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.justina.learningapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Vote(View view){
+        Intent intent = new Intent(this, Vote.class);
+        startActivity(intent);
+    }
+
+    public void viewMonthlyCharts(View view){
+        Intent intent = new Intent(this, ViewCharts.class);
+        startActivity(intent);
+    }
+
+    public void getHelp(View view){
+        Intent intent = new Intent(this, Contact.class);
+        startActivity(intent);
+    }
+
+    public void viewPrizes(View view){
+        Intent intent = new Intent(this, ViewPrizes.class);
+        startActivity(intent);
+    }
+
 }
---
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/Vote.java
@@ -0,0 +1,16 @@
+package com.example.justina.learningapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Vote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote);
+    }
+
+
+}

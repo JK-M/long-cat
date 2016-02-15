128d951356a4571d9245c15614e3497ddf1f8cf7
JK-M <stipriojikojyte@gmail.com>
2016-02-15 22:16:26 +0200

    Clickable items of a grid have been implemented on fragment 1. Fragment 2 now displays text depending on the  month clicked on the fragment1.

---
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ImageAdapter.java
@@ -0,0 +1,54 @@
+package com.example.justina.learningapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 110));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(10, 10, 10, 10);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to images
+    private Integer[] mThumbIds = {
+            R.drawable.month_1, R.drawable.month_2, R.drawable.month_3,
+            R.drawable.month_4, R.drawable.month_5, R.drawable.month_6,
+            R.drawable.month_7, R.drawable.month_8, R.drawable.month_9,
+            R.drawable.month_10, R.drawable.month_11, R.drawable.month_12
+    };
+}
--- LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
@@ -1,10 +1,15 @@
 package com.example.justina.learningapp;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewCharts extends AppCompatActivity {
 
@@ -13,10 +18,26 @@ public class ViewCharts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_charts);
 
+        // Content for the first fragment
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        // Second fragment
         TopChart chartFragment = new TopChart();
         FragmentManager fManager = getFragmentManager();
         FragmentTransaction fTransaction = fManager.beginTransaction();
         fTransaction.add(R.id.chart_activity_layout,chartFragment,"chartFragment");
         fTransaction.commit();
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+                int monthNo = position+1;
+                TextView textValue = (TextView) findViewById(R.id.month_chart);
+                textValue.setText("This is TOP10 of the month No. " + monthNo);
+            }
+        });
+
     }
 }

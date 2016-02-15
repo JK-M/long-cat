6cdc682eadda78508ffe5988b2aaa73b0c47453c
JK-M <stipriojikojyte@gmail.com>
2016-02-15 14:24:25 +0200

    Second fragment is now added programmatically.

--- LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
@@ -1,5 +1,8 @@
 package com.example.justina.learningapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,11 @@ public class ViewCharts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_charts);
+
+        TopChart chartFragment = new TopChart();
+        FragmentManager fManager = getFragmentManager();
+        FragmentTransaction fTransaction = fManager.beginTransaction();
+        fTransaction.add(R.id.chart_activity_layout,chartFragment,"chartFragment");
+        fTransaction.commit();
     }
 }

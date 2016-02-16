b5ef5c74ae5e09cb5517b728171e5b199e6ae4e1
JK-M <stipriojikojyte@gmail.com>
2016-02-16 15:17:11 +0200

    ListView has been implemented in order to show charts of the month.

--- LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ViewCharts.java
@@ -2,16 +2,35 @@ package com.example.justina.learningapp;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
-import android.widget.RemoteViews;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewCharts extends AppCompatActivity {
+
+    public Map<Integer, String> months;{
+        months = new HashMap<>();
+        months.put( 1,"January");
+        months.put( 2,"February");
+        months.put( 3,"March");
+        months.put( 4,"April");
+        months.put( 5,"May");
+        months.put( 6,"June");
+        months.put( 7,"July");
+        months.put( 8,"August");
+        months.put( 9,"September");
+        months.put(10,"October");
+        months.put(11,"November");
+        months.put(12,"December");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +51,35 @@ public class ViewCharts extends AppCompatActivity {
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-
-                int monthNo = position+1;
-                TextView textValue = (TextView) findViewById(R.id.month_chart);
-                textValue.setText("This is TOP10 of the month No. " + monthNo);
+                int monthNo = position + 1;
+                populateTopList(monthNo);
             }
         });
-
     }
+
+    public void populateTopList(int monthNo) {
+        TextView textValue = (TextView) findViewById(R.id.month_chart);
+        textValue.setText("TranceFix TOP10 of " + months.get(monthNo));
+
+        Resources topTen = getResources();
+        String[] tracks;
+
+        if (monthNo == 1){
+            tracks = topTen.getStringArray(R.array.top10_jan);
+        }
+        else if (monthNo == 2) {
+            tracks = topTen.getStringArray(R.array.top10_feb);
+        }
+        else if (monthNo == 3) {
+            tracks = topTen.getStringArray(R.array.top10_mar);
+        }
+        else {
+            tracks = topTen.getStringArray(R.array.top10_apr);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.top10_list, tracks);
+        ListView trackList = (ListView) findViewById(R.id.topTenList);
+        trackList.setAdapter(adapter);
+    }
+
 }

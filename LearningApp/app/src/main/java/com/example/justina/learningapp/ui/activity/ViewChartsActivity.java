92198e1918cc4d4962e4583bb1655f2a9ded0077
Vykintas Valkaitis <vykintas.valkaitis@affecto.com>
2016-02-19 13:11:44 +0200

    Refactored fragment usage logic

--- LearningApp/app/src/main/java/com/example/justina/learningapp/ui/activity/ViewChartsActivity.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ui/activity/ViewChartsActivity.java
@@ -1,90 +1,33 @@
 package com.example.justina.learningapp.ui.activity;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
-import com.example.justina.learningapp.data.adapter.ImageAdapter;
 import com.example.justina.learningapp.R;
+import com.example.justina.learningapp.ui.fragment.MonthListFragment;
 import com.example.justina.learningapp.ui.fragment.TopChartFragment;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ViewChartsActivity extends AppCompatActivity {
-
-    public Map<Integer, String> months;{
-        months = new HashMap<>();
-        months.put( 1,"January");
-        months.put( 2,"February");
-        months.put( 3,"March");
-        months.put( 4,"April");
-        months.put( 5,"May");
-        months.put( 6,"June");
-        months.put( 7,"July");
-        months.put( 8,"August");
-        months.put( 9,"September");
-        months.put(10,"October");
-        months.put(11,"November");
-        months.put(12,"December");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_charts);
 
-        // Content for the first fragment
-        GridView gridview = (GridView) findViewById(R.id.gridView);
-        gridview.setAdapter(new ImageAdapter(this));
+        // top charts fragment
+        TopChartFragment topChartFragment = TopChartFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.view_charts_top_chart_holder, topChartFragment, TopChartFragment.FRAGMENT_TAG)
+                .commitAllowingStateLoss();
 
-        // Second fragment
-        TopChartFragment chartFragment = new TopChartFragment();
-        FragmentManager fManager = getFragmentManager();
-        FragmentTransaction fTransaction = fManager.beginTransaction();
-        fTransaction.add(R.id.chart_activity_layout,chartFragment,"chartFragment");
-        fTransaction.commit();
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                int monthNo = position + 1;
-                populateTopList(monthNo);
-            }
-        });
-    }
-
-    public void populateTopList(int monthNo) {
-        TextView textValue = (TextView) findViewById(R.id.month_chart);
-        textValue.setText("TranceFix TOP10 of " + months.get(monthNo));
-
-        Resources topTen = getResources();
-        String[] tracks;
-
-        if (monthNo == 1){
-            tracks = topTen.getStringArray(R.array.top10_jan);
-        }
-        else if (monthNo == 2) {
-            tracks = topTen.getStringArray(R.array.top10_feb);
-        }
-        else if (monthNo == 3) {
-            tracks = topTen.getStringArray(R.array.top10_mar);
-        }
-        else {
-            tracks = topTen.getStringArray(R.array.top10_apr);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.top10_list, tracks);
-        ListView trackList = (ListView) findViewById(R.id.topTenList);
-        trackList.setAdapter(adapter);
+        // month list fragment
+        MonthListFragment monthListFragment = new MonthListFragment();
+        monthListFragment.setOnMonthSelectedListener(topChartFragment);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.view_charts_month_list_holder, monthListFragment, MonthListFragment.FRAGMENT_TAG)
+                .commitAllowingStateLoss();
     }
 
 }
--- LearningApp/app/src/main/java/com/example/justina/learningapp/ui/fragment/TopChartFragment.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ui/fragment/TopChartFragment.java
@@ -1,18 +1,92 @@
 package com.example.justina.learningapp.ui.fragment;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.justina.learningapp.R;
 
 
-public class TopChartFragment extends Fragment {
+public class TopChartFragment extends Fragment implements MonthListFragment.OnMonthSelectedListener {
+
+    public static String FRAGMENT_TAG = TopChartFragment.class.getName();
+
+    private TextView viewTopChartTitle;
+    private ListView viewTopChartList;
+
+    private ArrayAdapter<String> adapter;
+
+    public TopChartFragment() {
+    }
+
+    public static TopChartFragment newInstance() {
+        final TopChartFragment fragment = new TopChartFragment();
+
+        final Bundle arguments = new Bundle();
+
+        fragment.setArguments(arguments);
+
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_top_chart, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewTopChartTitle = (TextView) view.findViewById(R.id.top_chart_title);
+        viewTopChartList = (ListView) view.findViewById(R.id.top_chart_list);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        adapter = new ArrayAdapter<>(getActivity(), R.layout.top10_list);
+        viewTopChartList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        viewTopChartTitle = null;
+        viewTopChartList = null;
+    }
+
+    @Override
+    public void onMonthSelected(int id, String value) {
+        viewTopChartTitle.setText("TranceFix TOP10 of " + value);
+
+        Resources resources = getResources();
+        String[] tracks;
+
+        switch (id) {
+            case 1:
+                tracks = resources.getStringArray(R.array.top10_jan);
+                break;
+            case 2:
+                tracks = resources.getStringArray(R.array.top10_feb);
+                break;
+            case 3:
+                tracks = resources.getStringArray(R.array.top10_mar);
+                break;
+            default:
+                tracks = resources.getStringArray(R.array.top10_apr);
+        }
+
+        adapter.clear();
+        adapter.addAll(tracks);
     }
 }

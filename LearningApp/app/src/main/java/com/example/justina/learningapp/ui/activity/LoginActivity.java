c12b3131ae8a968dbcc989f7d272e071e8b215ca
Vykintas Valkaitis <vykintas.valkaitis@affecto.com>
2016-02-19 12:27:12 +0200

    Replace registration onclick listener implementation

--- LearningApp/app/src/main/java/com/example/justina/learningapp/ui/activity/LoginActivity.java
+++ LearningApp/app/src/main/java/com/example/justina/learningapp/ui/activity/LoginActivity.java
@@ -2,8 +2,8 @@ package com.example.justina.learningapp.ui.activity;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.justina.learningapp.R;
@@ -14,11 +14,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void registerOnTf(View view){
-        Intent registerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.trancefix.nl/register.php"));
-        startActivity(registerIntent);
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.trancefix.nl/register.php"));
+                startActivity(registerIntent);
+            }
+        });
     }
 
 }
